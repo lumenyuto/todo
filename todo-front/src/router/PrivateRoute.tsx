65a7a56d54607027f06a6ca0c2de715051605feb
lumenyuto <t.yuto200504@gmail.com>
2026-03-04 05:55:32 +0900

    update

--- todo-front/src/router/PrivateRoute.tsx
+++ todo-front/src/router/PrivateRoute.tsx
@@ -2,7 +2,7 @@ import type { FC, ReactNode } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
 export const PrivateRoute: FC<{children: ReactNode}> = ({children}) => {
-  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0()
+  const { isAuthenticated, loginWithRedirect } = useAuth0()
 
   if (isAuthenticated) {
     loginWithRedirect()

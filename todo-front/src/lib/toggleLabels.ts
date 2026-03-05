43517aef24279dab5d2ab3b53db8308dac6b8594
lumenyuto <t.yuto200504@gmail.com>
2026-03-05 13:38:25 +0900

    update

--- todo-front/src/lib/toggleLabels.ts
+++ todo-front/src/lib/toggleLabels.ts
@@ -2,5 +2,5 @@ import type { Label } from '../types/label'
 
 export const toggleLabels = (labels: Label[], target: Label) =>
   labels.find(({ id }) => id === target.id)
-    ? labels.filter(({ id }) => id === target.id)
+    ? labels.filter(({ id }) => id !== target.id)
     : [...labels, target]
